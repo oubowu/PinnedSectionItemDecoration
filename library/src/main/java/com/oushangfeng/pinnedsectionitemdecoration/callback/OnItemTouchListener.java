2a1af79fdc5cb24a17df2b68d0187ff915bfb4f4
oubowu <wwwbryant@163.com>
2018-11-08 01:12:11 +0800

    1.修复头部单击和长按的冲突 2.修复头部存在多个点击控件的冲突

--- library/src/main/java/com/oushangfeng/pinnedsectionitemdecoration/callback/OnItemTouchListener.java
+++ library/src/main/java/com/oushangfeng/pinnedsectionitemdecoration/callback/OnItemTouchListener.java
@@ -106,16 +106,6 @@ public class OnItemTouchListener implements RecyclerView.OnItemTouchListener {
         mGestureDetector.setIsLongpressEnabled(true);
         mGestureDetector.onTouchEvent(event);
 
-        //        if (event.getAction() == MotionEvent.ACTION_UP && !mIntercept && mDownInside) {
-        //            // 针对在头部滑动然后抬起手指的情况，如果在头部范围内需要拦截
-        //            float downX = event.getX();
-        //            float downY = event.getY();
-        //            final ClickBounds bounds = mBoundsArray.valueAt(0);
-        //            return downX >= bounds.getLeft() && downX <= bounds.getRight() && downY >= bounds.getTop() && downY <= bounds.getBottom();
-        //        }
-
-        // Log.i(TAG, "onInterceptTouchEvent(): " + mIntercept);
-
         return mIntercept;
     }
 
@@ -148,16 +138,16 @@ public class OnItemTouchListener implements RecyclerView.OnItemTouchListener {
         // 如果坐标在标签的范围内的话就屏蔽事件，自己处理
         //  mIntercept = downX >= mLeft && downX <= mRight && downY >= mTop && downY <= mBottom;
 
-        Log.i(TAG, " xy坐标: " + downX + ";" + downY);
+        // Log.i(TAG, " xy坐标: " + downX + ";" + downY);
 
         for (int i = 0; i < mBoundsArray.size(); i++) {
             // 逐个View拿出，判断坐标是否落在View的范围里面
             final ClickBounds bounds = mBoundsArray.valueAt(i);
-            Log.i(TAG, "  逐个View拿出: " + bounds.toString());
+            // Log.i(TAG, "  逐个View拿出: " + bounds.toString());
 
             boolean inside = downX >= bounds.getLeft() && downX <= bounds.getRight() && downY >= bounds.getTop() && downY <= bounds.getBottom();
             if (inside) {
-                Log.i(TAG, "  在点击范围内: " + inside);
+                // Log.i(TAG, "  在点击范围内: " + inside);
                 // 拦截事件成立
                 mIntercept = true;
                 // 点击范围内
@@ -178,10 +168,10 @@ public class OnItemTouchListener implements RecyclerView.OnItemTouchListener {
             mTmpClickId = mBoundsArray.keyAt(mBoundsArray.indexOfValue(mTmpBounds));
             mTmpView = mTmpBounds.getView();
             mTmpBounds = null;
-            Log.i(TAG, " 有点击中的: " + mTmpView);
+            // Log.i(TAG, " 有点击中的: " + mTmpView);
         }
 
-        Log.i(TAG, "OnRecyclerItemTouchListener-judge(): " + (mIntercept ? "屏蔽" : "不屏蔽"));
+        // Log.i(TAG, "OnRecyclerItemTouchListener-judge(): " + (mIntercept ? "屏蔽" : "不屏蔽"));
 
     }
 
@@ -191,19 +181,6 @@ public class OnItemTouchListener implements RecyclerView.OnItemTouchListener {
         public boolean onDown(MotionEvent e) {
 
             Log.i(TAG, "GestureListener-onDown(): ");
-
-            // 记录手指触碰，是否在头部范围内
-            // float downX = e.getX();
-            // float downY = e.getY();
-            // final ClickBounds bounds = mBoundsArray.valueAt(0);
-            // mDownInside = downX >= bounds.getLeft() && downX <= bounds.getRight() && downY >= bounds.getTop() && downY <= bounds.getBottom();
-
-            //            if (!mDoubleTap) {
-            //                mIntercept = false;
-            //            } else {
-            //                // 因为双击会在onDoubleTap后再调用onDown，所以这里要忽略第二次防止mIntercept被影响
-            //                mDoubleTap = false;
-            //            }
 
             shouldIntercept(e);
 
@@ -266,47 +243,6 @@ public class OnItemTouchListener implements RecyclerView.OnItemTouchListener {
             return mIntercept;
 
         }
-
-        //        @Override
-        //        public boolean onSingleTapConfirmed(MotionEvent e) {
-        //            Log.i(TAG, "GestureListener-113行-onSingleTapConfirmed(): ");
-        //
-        //            if (!mDisableHeaderClick && mIntercept && mHeaderClickListener != null && mAdapter != null && mPosition <= mAdapter.getItemCount() - 1) {
-        //                // 自己处理点击标签事件
-        //                try {
-        //                    mHeaderClickListener.onHeaderClick(mTmpView, mTmpClickId, mPosition);
-        //                } catch (IndexOutOfBoundsException e1) {
-        //                    e1.printStackTrace();
-        //                    //Log.i("TAG", "GestureListener-183行-onSingleTapConfirmed(): " + e1);
-        //                }
-        //            }
-        //
-        //            return super.onSingleTapConfirmed(e);
-        //        }
-        //
-        //        @Override
-        //        public boolean onDoubleTap(MotionEvent e) {
-        //
-        //            Log.i(TAG, "GestureListener-89行-onDoubleTap(): ");
-        //
-        //            // mDoubleTap = true;
-        //            shouldIntercept(e);
-        //
-        //            if (!mDisableHeaderClick && mIntercept && mHeaderClickListener != null && mAdapter != null && mPosition <= mAdapter.getItemCount() - 1) {
-        //                // 自己处理点击标签事件
-        //                try {
-        //                    mHeaderClickListener.onHeaderDoubleClick(mTmpView, mTmpClickId, mPosition);
-        //                } catch (IndexOutOfBoundsException e1) {
-        //                    e1.printStackTrace();
-        //                    //Log.i("TAG", "GestureListener-207行-onDoubleTap(): " + e1);
-        //                }
-        //            }
-        //
-        //            // 有机型在调用onDoubleTap后会接着调用onLongPress，这里这样处理
-        //            mGestureDetector.setIsLongpressEnabled(false);
-        //
-        //            return mIntercept;
-        //        }
 
     }
 
